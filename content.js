3db6be1071b8f7ae4591b8b6879e638b77c7400e
mirusu400 <mirusu400@naver.com>
2021-11-16 15:56:00 +0900

    Add detecting old tempermonkey script

--- content.js
+++ content.js
@@ -85,16 +85,24 @@ async function main() {
 	'use strict';
   const waitTimer = setInterval(() => {
     if (document.querySelector('.navtxt span:nth-child(1)')) {
-      document.querySelector('.navtxt span:nth-child(1)').innerHTML  = (`
-      <span style="margin-right: 20px">
+      if (document.querySelector('.navtxt span:nth-child(2)')) {
+        document.querySelector('.navtxt span:nth-child(2)').remove();
+        document.querySelector('.navtxt span:nth-child(1)').innerHTML  = (`
+        <a href="https://github.com/klas-helper/klas-helper-extension/wiki/%EA%B8%B0%EC%A1%B4-KLAS-Helper-Tempermonkey-%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-%EC%82%AD%EC%A0%9C-%EB%B0%A9%EB%B2%95"
+          target="_blank" rel="noopener" style="color: red; text-shadow: -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff;">
+            기존 스크립트를 삭제해 주세요! (클릭) 
+        </a>
+        `);
+      } else {
+        document.querySelector('.navtxt span:nth-child(1)').innerHTML  = (`
         <a href="https://github.com/klas-helper/klas-helper" target="_blank" rel="noopener">KLAS Helper</a>
         <a href="https://github.com/klas-helper/klas-helper-extension" target="_blank" rel="noopener">확장 프로그램</a>
         사용 중
-      </span>
-    `);
+        `);
+      }
       clearInterval(waitTimer);
     }
-  }, 100);
+  }, 500);
  
   
   
@@ -128,11 +136,26 @@ async function main() {
     
     scriptElement.src = jsCache(jsfile);
     document.head.appendChild(scriptElement);
-    for (const path in internalPathFunctions) {
-      if (path === location.pathname) {
-        internalPathFunctions[path]();
+    let useTempermonkey = false;
+    for (let i = 0; i < document.head.children.length; i++) {
+      if (document.head.children[i].src !== undefined) {
+        if (document.head.children[i].src.includes("https://klas-helper.github.io/klas-helper/dist/main.js")) {
+          console.log("Detect tempermonkey script!");
+          useTempermonkey = true;
+          break;
+        }
       }
     }
+
+    if (!useTempermonkey) {
+      document.head.appendChild(scriptElement);
+      for (const path in internalPathFunctions) {
+        if (path === location.pathname) {
+          internalPathFunctions[path]();
+        }
+      }
+    }
+    
   
     // 일정 시간이 지날 경우 타이머 해제
     setTimeout(() => {
