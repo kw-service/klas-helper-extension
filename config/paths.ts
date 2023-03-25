59760a27206b92bb93c0614f73ebfb55699a44e4
Naki <nbsp1221@gmail.com>
2023-03-26 04:13:42 +0900

    🧑‍💻 Remove unused codes and improve build process

--- config/paths.ts
+++ config/paths.ts
@@ -7,17 +7,13 @@ interface Paths {
   appDev: string;
   appDist: string;
   appSrc: string;
-  appUserscript: string;
-  appMain: string;
   appExtMain: string;
 }
 
 const paths: Paths = {
   appDev: resolveApp('dev'),
-  appDist: resolveApp('dist'),
+  appDist: resolveApp('core'),
   appSrc: resolveApp('src'),
-  appUserscript: resolveApp('src/klas-helper.user.js'),
-  appMain: resolveApp('src/main.js'),
   appExtMain: resolveApp('src/main-ext.js')
 };
 
--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -1,9 +1,9 @@
 import webpack from 'webpack';
-import WebpackUserscript from 'webpack-userscript';
 import { isEnvProduction } from './env';
 import paths from './paths';
 
-const commonConfig: webpack.Configuration = {
+const config: webpack.Configuration = {
+  entry: paths.appExtMain,
   mode: isEnvProduction ? 'production' : 'development',
   target: 'web',
   resolve: {
@@ -31,55 +31,11 @@ const commonConfig: webpack.Configuration = {
         }
       }
     ]
-  }
-};
-
-const userscriptConfig: webpack.Configuration = {
-  name: 'userscript',
-  entry: paths.appUserscript,
-  plugins: [
-    new WebpackUserscript({
-      headers: {
-        name: isEnvProduction ? 'KLAS Helper' : 'KLAS Helper Development',
-        namespace: 'https://github.com/nbsp1221',
-        match: 'https://klas.kw.ac.kr/*',
-        updateURL: 'https://openuserjs.org/meta/nbsp1221/KLAS_Helper.meta.js',
-        downloadURL: 'https://openuserjs.org/install/nbsp1221/KLAS_Helper.user.js',
-        grant: 'GM.xmlHttpRequest',
-        copyright: '2020, nbsp1221 (https://openuserjs.org/users/nbsp1221)',
-        license: 'MIT',
-        'run-at': 'document-start'
-      }
-    })
-  ],
-  optimization: {
-    minimize: false
   },
-  output: {
-    filename: 'klas-helper.js',
-    path: isEnvProduction ? paths.appDist : paths.appDev
-  },
-  ...commonConfig
-};
-
-const mainConfig: webpack.Configuration = {
-  name: 'main',
-  entry: paths.appMain,
-  output: {
-    filename: 'main.js',
-    path: paths.appDist
-  },
-  ...commonConfig
-};
-
-const mainExtConfig: webpack.Configuration = {
-  name: 'main-ext',
-  entry: paths.appExtMain,
   output: {
     filename: 'main-ext.js',
     path: paths.appDist
   },
-  ...commonConfig
 };
 
-export default [userscriptConfig, mainConfig, mainExtConfig];
+export default config;
