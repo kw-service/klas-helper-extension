00cf7dad530fb03407c7b2a752be291648536e71
Kim Taehwan <th.kim@kw.ac.kr>
2025-09-07 18:39:44 +0900

    Modified browser.ts to use object chrome instead of window.chrome or window.browser, both of which is undefined in Firefox

--- src/core/browser.ts
+++ src/core/browser.ts
@@ -1,1 +1,1 @@
-export const browser = typeof window !== 'undefined' ? (window.browser || window.chrome) : chrome;
+export const browser = (typeof window !== 'undefined') ? ((typeof window.chrome !== 'undefined' || typeof window.browser !== 'undefined') ? (window.browser || window.chrome) : chrome) : chrome;
