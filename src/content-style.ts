4be59eac6279bb64f54c25f0c30dc3b2c62ae0cc
sooseongcom <sooseongcom@gmail.com>
2026-07-02 18:34:59 +0900

    :sparkles: 이미지 크기 조정 기능 추가
    * Popup 창에서 '이미지 크기 조정'을 켜면 아래와 같이 작동합니다.
     * 이미지 max-width와 max-height가 100%로 변경됩니다.(이미지 크기가 너무 커서 모니터 밖으로 빠져나가는 경우 사용)
     * 이미지에 마우스 커서를 올리면 돋보기 모양으로 바뀌며, 이미지를 클릭하면 이미지 원본을 볼 수 있습니다.

--- src/content-style.ts
+++ src/content-style.ts
@@ -35,6 +35,44 @@ function main() {
 
       }
 
+      // 크롬 sync 스토리지 이용해 체크 여부 확인
+      try {
+        // 이미지 크기 조정 적용
+        browser.storage.sync.get(null).then((items) => {
+          // chrome namespace not supported
+          if (items.useImgSize === undefined) {
+            browser.storage.sync.set({ 'useImgSize': 'OFF' });
+          }
+          else if (items.useImgSize === 'ON') {
+            //   이미지 크기 조정 css 삽입
+            const style = document.createElement('link');
+            style.href = browser.runtime.getURL('imgSize.css');
+            style.type = 'text/css';
+            style.rel = 'stylesheet';
+            document.querySelector('head')?.append(style);
+
+            //   이미지 클릭 시 확대
+            document.addEventListener('click', function (event) {
+              if (event.target instanceof HTMLImageElement) {
+                if (event.target.tagName === 'IMG') {
+                  event.preventDefault();
+
+                  let win = window.open('') as Window;
+                  win.document.title = '이미지 원본 보기';
+                  win.document.body.innerHTML = '<div id="button"><button id="close">이미지 닫기 Close image</button></div> <div id="imgview"><img src="' + event.target.src + '" alt="show-img-big"/></div> <style>#button{width: 100%; height: 5%;} button{width: 100%; height: 90%; font-size: 2.5vh; background-color: #666666; color: #fff; border: 1px solid #666; border-bottom: 1px solid #555; border-right: 1px solid #5b5b5b; cursor: pointer;} #imgview{width: 100%; height: 95%;} img{object-fit: contain;} </style>';
+                  win.document.getElementById('close')?.addEventListener('click', () => win.close());
+                }
+              }
+            }, true);
+          }
+        }).catch((error) => {
+          console.error('Error accessing storage:', error);
+        });
+      }
+      catch (e) {
+
+      }
+
       document_observer.disconnect();
     }
   });
